84d608235dd63a3f32c667d1f7807cf394ae7104
Roy Hernandez <royhernandez@MacBook-Air.local>
2019-11-24 09:05:54 -0800

    input fields

---
+++ index.js
@@ -0,0 +1,6 @@
+function sum(num1, num2) {
+    var num1 = document.getElementById("num1").value;
+    var num2 = document.getElementById("num2").value;
+    result =  parseInt(num1) + parseInt(num2);
+    document.getElementById("addResult").value = result
+ }
